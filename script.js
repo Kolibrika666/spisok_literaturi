8d60a8e14e9cb62e53ca49d82b95e3a27689b305
Kolibrika666 <140741201+Kolibrika666@users.noreply.github.com>
2023-08-10 15:41:33 +0700

    animation

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 "use strict"; // этот код работает в современном режиме
 
 
-//скрываем главный элемент, чтобы при нажатии на кнопку он исчезал (не работает)
-const show = () => {
-  document.getElementById(glava).style.display = 'none';
-  }
+function push() {
+  if (Element.style.display == 'block') Element.style.display = 'none';
+  else Element.style.display = 'block';
+  return false;
+};
 
-  const button_a = document.querySelector('button a');
-  button_a.addEventListener('click', show);
+const blosk = getElementById("glava");
+
+
+//function myMove() {
+ // var elem = document.getElementById("animation_arrow");
+ // var pos = 0;
+ // var id = setInterval(frame, 10);
+ // function frame() {
+      //if (pos == 350) {
+       //   clearInterval(id);
+     // } else {
+         // pos++;
+       //   elem.style.top = pos + 'px';
+        //  elem.style.left = pos + 'px';
+     // }
+ // }
+//}
+
+
+
+//function push() {
+ // var shadow = document.getElementById ('glava');
+  //shadow.style.display="none";
+//  };
+
+//const knopka = document.getElementById ('button_a');
+//knopka.addEventListener('click', push);
